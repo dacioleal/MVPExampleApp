4911e1249130c639ac6636624760e5c393d5e651
Dacio Leal <dacioleal@gmail.com>
2019-12-27 10:39:36 +0100

    Refactoring to build the URLSession calling one method

--- MVPExampleApp/DataManager.swift
+++ MVPExampleApp/DataManager.swift
@@ -14,71 +14,53 @@ class DataManager {
         print("Error: \(error)")
     }
     
+    //Builds generic URLSession to get an array of objects
+    static func executeURLSessionDataTaskForArrayJSON<T:Codable>(request: URLRequest, completion: @escaping (_ results:[T]?) -> Void) {
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(nil)
+                if let error = error {
+                    handleServerError(error: error)
+                }
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            do {
+                let elements = try JSONDecoder().decode([T].self, from: data)
+                completion(elements)
+            } catch {
+                print("JSON parsing error: \(error) - for service: \(request.url!)")
+            }
+        }
+        task.resume()
+    }
+    
     
     //Gets the list of conferences as an array of Conference objects from the api service
     static func getCollegeFootballConferences(completion: @escaping (_ results:[Conference]?) -> Void) {
-        
         let url = URL(string: "https://api.collegefootballdata.com/conferences")
         var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 60.0)
         request.httpMethod = "GET"
         
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                completion(nil)
-                if let error = error {
-                    handleServerError(error: error)
-                }
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil)
-                return
-            }
-            
-            do {
-                let conferences = try JSONDecoder().decode([Conference].self, from: data)
-                completion(conferences)
-            } catch {
-                print("JSON parsing error: \(error) - for service: \(url!.absoluteString)")
-            }
+        executeURLSessionDataTaskForArrayJSON(request: request) { (results:[Conference]?) in
+            completion(results)
         }
-        
-        task.resume()
     }
     
     //Gets the list of teams from a conference as an array of Team objects from the api service
     static func getTeamsFromConference(conferenceAbbreviation: String, completion: @escaping (_ results:[Team]?) -> Void) {
-        
         let url = URL(string: "https://api.collegefootballdata.com/teams?conference=" + conferenceAbbreviation)
         var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 60.0)
         request.httpMethod = "GET"
         
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                completion(nil)
-                if let error = error {
-                    handleServerError(error: error)
-                }
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil)
-                return
-            }
-            
-            do {
-                let teams = try JSONDecoder().decode([Team].self, from: data)
-                completion(teams)
-            } catch {
-                print("JSON parsing error: \(error) - for service: \(url!.absoluteString)")
-            }
+        executeURLSessionDataTaskForArrayJSON(request: request) { (results:[Team]?) in
+            completion(results)
         }
-        
-        task.resume()
     }
-    
 }
